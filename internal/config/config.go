9da22dcb158a080a931c9ca8a0256428e203e14b
byhat <goggssg3@mail.ru>
2025-07-21 15:52:40 +0300

    linter errors fix

--- internal/config/config.go
+++ internal/config/config.go
@@ -9,10 +9,10 @@ import (
 )
 
 type Config struct {
-	Host  string
-	Port  int
-	Debug bool
-	DBDSN string
+	Host  string `json:"host"`
+	Port  int    `json:"port"`
+	Debug bool   `json:"debug"`
+	DBDSN string `json:"dbdsn"`
 }
 
 const (
@@ -29,75 +29,157 @@ func getConfigFromFile(path string) (*Config, error) {
 	defer file.Close()
 
 	var cfg Config
-	if err := json.NewDecoder(file).Decode(&cfg); err != nil {
+	if err = json.NewDecoder(file).Decode(&cfg); err != nil {
 		return nil, fmt.Errorf("unable decode data in config file: %w", err)
 	}
 
 	return &cfg, nil
 }
 
-func ReadConfig() (*Config, error) {
-	var cfg Config
-	var configFile string
-	var err error
-
+func parseFlags(cfg *Config, configFile *string) {
 	flag.StringVar(&cfg.Host, "host", defaultHost, "flag for explicit server host specifications")
 	flag.IntVar(&cfg.Port, "port", defaultPort, "flag for explicit server port specifications")
 	flag.BoolVar(&cfg.Debug, "debug", false, "flag for explicit debug mode")
 	flag.StringVar(&cfg.DBDSN, "db", defaultDBDSN, "flag for explicit database specifications")
 
-	// set path to config file
-	flag.StringVar(&configFile, "c", "", "path to config file (JSON format)")
-	flag.StringVar(&configFile, "config", "", "path to config file (JSON format)")
+	flag.StringVar(configFile, "c", "", "path to config file (JSON format)")
+	flag.StringVar(configFile, "config", "", "path to config file (JSON format)")
 
 	flag.Parse()
+}
 
-	// parse config file
-	if configFile != "" {
-		var fileCfg *Config
-		if fileCfg, err = getConfigFromFile(configFile); err != nil {
-			return nil, err
-		}
-
-		if cfg.Host == defaultHost {
-			cfg.Host = fileCfg.Host
-		}
-		if cfg.Port == defaultPort {
-			cfg.Port = fileCfg.Port
-		}
-		if !cfg.Debug {
-			cfg.Debug = fileCfg.Debug
-		}
-		if cfg.DBDSN == defaultDBDSN {
-			cfg.DBDSN = fileCfg.DBDSN
-		}
+func applyConfigFile(cfg *Config, path string) error {
+	if path == "" {
+		return nil
 	}
 
-	// parse env variables
-	if host := os.Getenv("HOST"); host != "" && cfg.Host != host {
+	fileCfg, err := getConfigFromFile(path)
+	if err != nil {
+		return err
+	}
+
+	if cfg.Host == defaultHost {
+		cfg.Host = fileCfg.Host
+	}
+	if cfg.Port == defaultPort {
+		cfg.Port = fileCfg.Port
+	}
+	if !cfg.Debug {
+		cfg.Debug = fileCfg.Debug
+	}
+	if cfg.DBDSN == defaultDBDSN {
+		cfg.DBDSN = fileCfg.DBDSN
+	}
+
+	return nil
+}
+
+func applyEnvVars(cfg *Config) error {
+	if host := os.Getenv("HOST"); host != "" {
 		cfg.Host = host
 	}
 	if portStr := os.Getenv("PORT"); portStr != "" {
 		port, err := strconv.Atoi(portStr)
 		if err != nil {
-			return nil, fmt.Errorf("invalid PORT value: %w", err)
+			return fmt.Errorf("invalid PORT value: %w", err)
 		}
-		if cfg.Port != port {
-			cfg.Port = port
-		}
+		cfg.Port = port
 	}
 	if debugStr := os.Getenv("DEBUG"); debugStr != "" {
 		debug, err := strconv.ParseBool(debugStr)
 		if err != nil {
-			return nil, fmt.Errorf("invalid DEBUG value: %w", err)
+			return fmt.Errorf("invalid DEBUG value: %w", err)
 		}
-		if cfg.Debug != debug {
-			cfg.Debug = debug
-		}
+		cfg.Debug = debug
 	}
-	if dbdsn := os.Getenv("DB_DSN"); dbdsn != "" && cfg.DBDSN != dbdsn {
+	if dbdsn := os.Getenv("DB_DSN"); dbdsn != "" {
 		cfg.DBDSN = dbdsn
 	}
-
-	return &cfg, nil
+	return nil
 }
+
+func ReadConfig() (*Config, error) {
+	cfg := &Config{
+		Host:  defaultHost,
+		Port:  defaultPort,
+		Debug: false,
+		DBDSN: defaultDBDSN,
+	}
+
+	var configFile string
+	parseFlags(cfg, &configFile)
+	if err := applyConfigFile(cfg, configFile); err != nil {
+		return nil, err
+	}
+	if err := applyEnvVars(cfg); err != nil {
+		return nil, err
+	}
+
+	return cfg, nil
+}
+
+// func ReadConfig() (*Config, error) {
+// 	var cfg Config
+// 	var configFile string
+// 	var err error
+
+// 	flag.StringVar(&cfg.Host, "host", defaultHost, "flag for explicit server host specifications")
+// 	flag.IntVar(&cfg.Port, "port", defaultPort, "flag for explicit server port specifications")
+// 	flag.BoolVar(&cfg.Debug, "debug", false, "flag for explicit debug mode")
+// 	flag.StringVar(&cfg.DBDSN, "db", defaultDBDSN, "flag for explicit database specifications")
+
+// 	// set path to config file
+// 	flag.StringVar(&configFile, "c", "", "path to config file (JSON format)")
+// 	flag.StringVar(&configFile, "config", "", "path to config file (JSON format)")
+
+// 	flag.Parse()
+
+// 	// parse config file
+// 	if configFile != "" {
+// 		var fileCfg *Config
+// 		if fileCfg, err = getConfigFromFile(configFile); err != nil {
+// 			return nil, err
+// 		}
+
+// 		if cfg.Host == defaultHost {
+// 			cfg.Host = fileCfg.Host
+// 		}
+// 		if cfg.Port == defaultPort {
+// 			cfg.Port = fileCfg.Port
+// 		}
+// 		if !cfg.Debug {
+// 			cfg.Debug = fileCfg.Debug
+// 		}
+// 		if cfg.DBDSN == defaultDBDSN {
+// 			cfg.DBDSN = fileCfg.DBDSN
+// 		}
+// 	}
+
+// 	// parse env variables
+// 	if host := os.Getenv("HOST"); host != "" && cfg.Host != host {
+// 		cfg.Host = host
+// 	}
+// 	if portStr := os.Getenv("PORT"); portStr != "" {
+// 		port, err := strconv.Atoi(portStr)
+// 		if err != nil {
+// 			return nil, fmt.Errorf("invalid PORT value: %w", err)
+// 		}
+// 		if cfg.Port != port {
+// 			cfg.Port = port
+// 		}
+// 	}
+// 	if debugStr := os.Getenv("DEBUG"); debugStr != "" {
+// 		debug, err := strconv.ParseBool(debugStr)
+// 		if err != nil {
+// 			return nil, fmt.Errorf("invalid DEBUG value: %w", err)
+// 		}
+// 		if cfg.Debug != debug {
+// 			cfg.Debug = debug
+// 		}
+// 	}
+// 	if dbdsn := os.Getenv("DB_DSN"); dbdsn != "" && cfg.DBDSN != dbdsn {
+// 		cfg.DBDSN = dbdsn
+// 	}
+
+// 	return &cfg, nil
+// }
