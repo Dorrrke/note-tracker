f6bf9d6c6074cf44d8e70a7de5ed826af7747d0a
byhat <goggssg3@mail.ru>
2025-07-21 15:37:17 +0300

    done

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,9 @@
 package config
 
 import (
-	"cmp"
+	"encoding/json"
 	"flag"
+	"fmt"
 	"os"
 	"strconv"
 )
@@ -20,35 +21,83 @@ const (
 	defaultDBDSN = "postgres://user:password@db:5432/note_tracker?sslmode=disable"
 )
 
+func getConfigFromFile(path string) (*Config, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, fmt.Errorf("unable open config file: %w", err)
+	}
+	defer file.Close()
+
+	var cfg Config
+	if err := json.NewDecoder(file).Decode(&cfg); err != nil {
+		return nil, fmt.Errorf("unable decode data in config file: %w", err)
+	}
+
+	return &cfg, nil
+}
+
 func ReadConfig() (*Config, error) {
 	var cfg Config
+	var configFile string
+	var err error
 
 	flag.StringVar(&cfg.Host, "host", defaultHost, "flag for explicit server host specifications")
 	flag.IntVar(&cfg.Port, "port", defaultPort, "flag for explicit server port specifications")
 	flag.BoolVar(&cfg.Debug, "debug", false, "flag for explicit debug mode")
 	flag.StringVar(&cfg.DBDSN, "db", defaultDBDSN, "flag for explicit database specifications")
 
+	// set path to config file
+	flag.StringVar(&configFile, "c", "", "path to config file (JSON format)")
+	flag.StringVar(&configFile, "config", "", "path to config file (JSON format)")
+
 	flag.Parse()
 
-	if cfg.Host == "localhost" {
-		cfg.Host = cmp.Or(os.Getenv("HOST"), cfg.Host)
-	}
-
-	if cfg.Port == defaultPort {
-		defPort := strconv.Itoa(cfg.Port)
-		envPort := cmp.Or(os.Getenv("PORT"), defPort)
-		port, err := strconv.Atoi(envPort)
-		if err != nil {
+	// parse config file
+	if configFile != "" {
+		var fileCfg *Config
+		if fileCfg, err = getConfigFromFile(configFile); err != nil {
 			return nil, err
 		}
-		cfg.Port = port
+
+		if cfg.Host == defaultHost {
+			cfg.Host = fileCfg.Host
+		}
+		if cfg.Port == defaultPort {
+			cfg.Port = fileCfg.Port
+		}
+		if !cfg.Debug {
+			cfg.Debug = fileCfg.Debug
+		}
+		if cfg.DBDSN == defaultDBDSN {
+			cfg.DBDSN = fileCfg.DBDSN
+		}
 	}
 
-	cfg.DBDSN = cmp.Or(os.Getenv("DB_DSN"), cfg.DBDSN)
-
-	// if cfg.MigratePath == defaultMigratePath {
-	// 	cfg.MigratePath = cmp.Or(os.Getenv("MIGRATE_PATH"), cfg.MigratePath)
-	// }
+	// parse env variables
+	if host := os.Getenv("HOST"); host != "" && cfg.Host != host {
+		cfg.Host = host
+	}
+	if portStr := os.Getenv("PORT"); portStr != "" {
+		port, err := strconv.Atoi(portStr)
+		if err != nil {
+			return nil, fmt.Errorf("invalid PORT value: %w", err)
+		}
+		if cfg.Port != port {
+			cfg.Port = port
+		}
+	}
+	if debugStr := os.Getenv("DEBUG"); debugStr != "" {
+		debug, err := strconv.ParseBool(debugStr)
+		if err != nil {
+			return nil, fmt.Errorf("invalid DEBUG value: %w", err)
+		}
+		if cfg.Debug != debug {
+			cfg.Debug = debug
+		}
+	}
+	if dbdsn := os.Getenv("DB_DSN"); dbdsn != "" && cfg.DBDSN != dbdsn {
+		cfg.DBDSN = dbdsn
+	}
 
 	return &cfg, nil
 }
